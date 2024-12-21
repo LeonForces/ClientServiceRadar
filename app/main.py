1c6ddf7d19c36c944d61483c471fea0d82195c79
leonforces <nb.abubakir@gmail.com>
2024-12-21 12:08:04 +0300

    add scheduler

--- app/main.py
+++ app/main.py
@@ -34,9 +34,6 @@ from datetime import datetime
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-
-    # await delete_tables()
-    # await create_tables()
     redis = aioredis.from_url(settings.redis_url, encoding="utf8", decode_responses=True)
     FastAPICache.init(RedisBackend(redis), prefix="cache")
 
